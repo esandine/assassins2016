0cd6a24111e4aff833bcf4af31656c1425c18c26
esandine <esandine@stuy.edu>
2016-10-30 14:41:22 -0400

    It all works out

--- kill.py
+++ kill.py
@@ -28,13 +28,33 @@ def kill():
     list=file.read()
     file.close()
     list=list.split(",")
-
     h=hashlib.md5()
     h.update(killer)
     killer=h.hexdigest()
+    #Finds hash of killed player and kills it
+    i =0
+    while i < len(list):
+        if killer == list[i]:
+            if i==len(list)-1:
+                list.pop(0)
+                target=list[0]
+                i=len(list)
+            elif i==len(list)-2:
+                list.pop(len(list)-1)
+                target=list[0]
+                i=len(list)
+            else:
+                list.pop(i+1)
+                target=list[i+1]
+                i=len(list)
+        i+=1
+    retstr=""
     for user in list:
-        if killer == user:
-            if(index(user)=len(list)
-            Killed.Pop(list.index(user)+1)
-    
+        retstr+=user
+        retstr+=","
+    retstr=retstr[:-1]
+    file = open("list.csv","w")
+    file.write(retstr)
+    file.close()
+
 kill()
